321a469d69f9302c7eca4145d50fc0760d876a41
Andrew Uteev <120500uas@gmail.com>
2025-05-29 16:11:07 +0300

    Откладка

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,15 +15,18 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	// Попытаться загрузить .env файл, но не ругаться, если его нет
-	// (Например, в продакшене .env не будет — и это нормально)
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using environment variables")
 	}
 
-	tgChatId, _ := strconv.Atoi(os.Getenv("TELEGRAM_CHAT_ID"))
+	token := os.Getenv("TELEGRAM_TOKEN")
+	chatIdStr := os.Getenv("TELEGRAM_CHAT_ID")
+	log.Printf("Telegram token: %s", token)       // для дебага
+	log.Printf("Telegram chat ID: %s", chatIdStr) // для дебага
+
+	tgChatId, _ := strconv.Atoi(chatIdStr)
 	return &Config{
-		TelegramToken:   os.Getenv("TELEGRAM_TOKEN"),
+		TelegramToken:   token,
 		TelegramChatId:  tgChatId,
 		QuoteAPIURL:     os.Getenv("QUOTE_API_URL"),
 		TranslateAPIURL: os.Getenv("TRANSLATE_API_URL"),
